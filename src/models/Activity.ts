356b1a92fc73f3b1310ae8dcc1c53e1fca3d8270
Carlos Roberto De Rolt Junior <carlosroltjr@gmail.com>
2021-05-02 01:15:31 -0300

    fix: adding courseUnitId attribute that was missing

--- src/models/Activity.ts
+++ src/models/Activity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn } from 'typeorm'
+import { Column, CreateDateColumn, Entity, Long, ManyToOne, PrimaryColumn } from 'typeorm'
 import { v4 as uuid } from 'uuid'
 
 import { CourseUnit } from './CourseUnit'
@@ -19,6 +19,9 @@ class Activity {
 
   @Column()
   activityDate: Date
+
+  @Column()
+  courseUnitId: string
 
   @ManyToOne(() => CourseUnit, courseUnit => courseUnit.activities)
   courseUnit: CourseUnit
