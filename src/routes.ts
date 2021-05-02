e778082c398c57b73a273ac22ab230e6db9778ba
Carlos Roberto De Rolt Junior <carlosroltjr@gmail.com>
2021-05-02 01:14:50 -0300

    feat: adding test routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express'
 
+interface UserRequest {
+  name: string
+  email: string
+  password: string
+}
+
 const routes = Router()
 
-routes.get('/user', (request, response) => response.json({
-  message: 'It is working, Hooray!'
-}))
+routes.get('/test', (request, response) => response.json({ message: 'It is working, Hooray!' }))
+
+routes.post('/user', () => console.log('User route'))
+routes.post('/activity', () => console.log('Activity route'))
+routes.post('/courseUnit', () => console.log('Course Unit route'))
 
 export default routes
